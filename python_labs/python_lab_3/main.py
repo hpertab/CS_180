a3c71261b97f1fd2ddc877cb13250c59ed6e820e
Hana Pertab <hpertab@glacier.cs.byu.edu>
2023-01-26 12:57:23 -0700

    Small Change

--- python_labs/python_lab_3/main.py
+++ python_labs/python_lab_3/main.py
@@ -3,9 +3,29 @@ import numpy
 import os
 import math
 
+def cube(n: int) -> int:
+    cube_sum = 0
+    i = 1
+    while i < n+1:
+        cur_cube = i**3 
+        cur_cube_copy = cur_cube
+        first_digit = cur_cube_copy
+        while (cur_cube_copy > 9):
+            cur_cube_copy = int(cur_cube_copy/10)
+            first_digit = int(cur_cube_copy%10)
+        if (first_digit%2 == 0):
+            cube_sum = cube_sum + cur_cube
+        i += 1
+    return cube_sum
+
 def main(number: int) -> int:
     # Write the code to sum up cubed numbers here.
-    # Make sure that your terminal output matches the terminal output of the example given on the instructions.
+    # Make sure that your terminal output matches the 
+    # terminal output of the example given on the instructions.
+
+    result = cube(number)
+
+    print("cube(%d) = %d" % (number, result))
 
     return None
 
