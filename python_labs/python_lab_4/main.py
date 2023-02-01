a582d5e8bee62c056a0fe5a9619eb39554f346c9
Hana Pertab <hpertab@acadia.cs.byu.edu>
2023-01-31 17:10:47 -0700

    Small Change

--- python_labs/python_lab_4/main.py
+++ python_labs/python_lab_4/main.py
@@ -1,8 +1,32 @@
 import argparse as ap
 
+def find_mean(array):
+    sum = 0.0
+    for cur_num in array:
+        sum += float(cur_num)
+    mean = float(sum / float(len(array)))
+    return mean
+
+def find_variance(array, mean):
+    sum = 0.0
+    for cur_num in array:
+        cur_var = float(cur_num) - mean
+        cur_var = cur_var**2
+        sum += float(cur_var)
+    variance = float(sum / float(len(array)))
+    return variance
+
 def main(array):
     # Write the compute the variance and the mean of a given list of numbers
-    # Make sure that your terminal output matches the terminal output of the example given on the instructions.
+
+    # Make sure that your terminal output matches the terminal output of the 
+    # example given on the instructions.
+
+    mean = find_mean(array)
+    variance = find_variance(array, mean)
+
+    print("mean =", round(mean, 16))
+    print("variance =", round(variance, 16))
 
     return None
 
