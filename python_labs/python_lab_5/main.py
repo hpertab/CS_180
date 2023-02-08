e30b3be6ccd1cd34052d93b0057a1b830f58ae8b
Hana Pertab <hpertab@hawaii.cs.byu.edu>
2023-02-07 17:05:48 -0700

    Small Change

--- python_labs/python_lab_5/main.py
+++ python_labs/python_lab_5/main.py
@@ -3,7 +3,13 @@ import argparse
 def main(inputString):
     # Write the code to reverse the string here
     # Make sure that your terminal output matches the terminal output of the example given on the instructions.
-    
+    string_list = list(inputString)
+    string_list.reverse()
+    string_reversed = ""
+    for cur_char in string_list:
+        string_reversed += cur_char
+    print(string_reversed)
+
     return None
 
 if __name__ == "__main__":
